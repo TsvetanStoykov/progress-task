ddeac1fc5dd0f4d4752ad4df785cae3f86a33446
Tsvetan Stoykov <45481296+TsvetanStoykov@users.noreply.github.com>
2019-08-19 00:14:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 
+// Draw table from 'jsData' array of objects 
+function drawTable(tbody) { var tr, td; 
+  tbody = document.getElementById(tbody); // loop through data source 
+  for (var i = 0; i < products.length; i++) 
+     { tr = tbody.insertRow(tbody.rows.length); 
+      td = tr.insertCell(tr.cells.length); 
+      td.setAttribute("align", "center"); 
+      td.innerHTML = products[i].ProductName; 
+      td = tr.insertCell(tr.cells.length); 
+      td.innerHTML = products[i].UnitsInStock; 
+      td = tr.insertCell(tr.cells.length); 
+      td.innerHTML = products[i].UnitPrice; 
+      td = tr.insertCell(tr.cells.length); 
+      td.innerHTML = products[i].Discontinued; 
+      td = tr.insertCell(tr.cells.length);} } 
 
-for (var i=0; i<products.length; i++){
+drawTable('table-data')
 
-  var productName = document.createElement('td')
-  productName.textContent = products[i].ProductName
-  document.querySelector('#product-name').appendChild(productName)
+// var table = document.getElementById("display-table"); //give this ID to your table
 
-  var unitsInStock = document.createElement('td')
-  unitsInStock.textContent = products[i].UnitsInStock
-  document.querySelector('#units-in-stock').appendChild(unitsInStock)
+// for (var i = 0; i < products.length; i++) {
+//     var row = table.insertRow(i);
+//     var cell = row.insertCell(0);
+//     cell.innerHTML = products[i].ProductName;
+// }
 
-  var unitPrice = document.createElement('td')
-  unitPrice.textContent = products[i].UnitPrice
-  document.querySelector('#unit-price').appendChild(unitPrice)
 
-  var discontinued = document.createElement('td')
-  discontinued.textContent = products[i].Discontinued
-  document.querySelector('#discontinued').appendChild(discontinued)
 
-  var btn = document.createElement('button');   
-  btn.innerHTML = "Details";                   
-  document.querySelector('#details').appendChild(btn);  
-}
+// for (var i=0; i<products.length; i++){
+
+//   var productName = document.createElement('td')
+//   productName.textContent = products[i].ProductName
+//   document.querySelector('#product-name').appendChild(productName)
+
+//   var unitsInStock = document.createElement('td')
+//   unitsInStock.textContent = products[i].UnitsInStock
+//   document.querySelector('#units-in-stock').appendChild(unitsInStock)
+
+//   var unitPrice = document.createElement('td')
+//   unitPrice.textContent = products[i].UnitPrice
+//   document.querySelector('#unit-price').appendChild(unitPrice)
+
+//   var discontinued = document.createElement('td')
+//   discontinued.textContent = products[i].Discontinued
+//   document.querySelector('#discontinued').appendChild(discontinued)
+
+//   var detailsBtn = document.createElement('td') 
+//   detailsBtn.textContent = ('View Details')  
+//   document.querySelector('#details').appendChild(detailsBtn)
+  
+// }
 
