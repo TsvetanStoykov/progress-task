71b3a6630fa5aafd5003f9a283b6f86b65ff166e
Tsvetan Stoykov <45481296+TsvetanStoykov@users.noreply.github.com>
2019-08-21 07:42:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
+// Draw table from 'products' array of objects 
 
-// Draw table from 'jsData' array of objects 
-function drawTable(tbody) { var tr, td; 
-  tbody = document.getElementById(tbody); // loop through data source 
-  for (var i = 0; i < products.length; i++) 
+function drawTable(tbody) { 
+  
+  var tr, td; 
+  tbody = document.getElementById(tbody); 
+
+  for (var i = 0; i < products.length; i++) // loop through data source
      { tr = tbody.insertRow(tbody.rows.length); 
       td = tr.insertCell(tr.cells.length); 
-      td.setAttribute("align", "center"); 
       td.innerHTML = products[i].ProductName; 
       td = tr.insertCell(tr.cells.length); 
       td.innerHTML = products[i].UnitsInStock; 
       td = tr.insertCell(tr.cells.length); 
-      td.innerHTML = products[i].UnitPrice; 
+      td.innerHTML = products[i].UnitPrice;
       td = tr.insertCell(tr.cells.length); 
-      td.innerHTML = products[i].Discontinued; 
-      td = tr.insertCell(tr.cells.length);} } 
+      
+      // Replaceing true/false values with disabled checkboxes
+      checkbox = document.createElement('input');
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.disabled = true;
+      checkbox.checked = products[i].Discontinued;
+      td.appendChild(checkbox);  
 
-drawTable('table-data')
+      // View Details button
+      td = tr.insertCell(tr.cells.length);  
+      button = document.createElement('button');
+      button.textContent = ('View Details');
 
-// var table = document.getElementById("display-table"); //give this ID to your table
+      // Display details on button click
+      button.onclick = (n=> event=> {
+        
+        const productName = document.querySelector('#product-name');
+        productName.textContent = products[n].ProductName;
 
-// for (var i = 0; i < products.length; i++) {
-//     var row = table.insertRow(i);
-//     var cell = row.insertCell(0);
-//     cell.innerHTML = products[i].ProductName;
-// }
+        const productCategory = document.querySelector('#product-category'); 
+        productCategory.textContent = products[n].Category.CategoryName;
+
+        const productDescription = document.querySelector('#product-description');
+        productDescription.textContent = products[n].Category.Description;
+        
+      })(i);
+      td.appendChild(button);
+      
+      } } 
+     
+drawTable('table-data');
 
 
 
-// for (var i=0; i<products.length; i++){
 
-//   var productName = document.createElement('td')
-//   productName.textContent = products[i].ProductName
-//   document.querySelector('#product-name').appendChild(productName)
-
-//   var unitsInStock = document.createElement('td')
-//   unitsInStock.textContent = products[i].UnitsInStock
-//   document.querySelector('#units-in-stock').appendChild(unitsInStock)
-
-//   var unitPrice = document.createElement('td')
-//   unitPrice.textContent = products[i].UnitPrice
-//   document.querySelector('#unit-price').appendChild(unitPrice)
-
-//   var discontinued = document.createElement('td')
-//   discontinued.textContent = products[i].Discontinued
-//   document.querySelector('#discontinued').appendChild(discontinued)
-
-//   var detailsBtn = document.createElement('td') 
-//   detailsBtn.textContent = ('View Details')  
-//   document.querySelector('#details').appendChild(detailsBtn)
-  
-// }
 
